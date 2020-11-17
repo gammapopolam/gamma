7fb1a17cdc6ae6cbf4fe60e480d031e5a8a2953f
gammapopolam <56549605+gammapopolam@users.noreply.github.com>
2020-11-17 20:07:35 +0300

    new density method
    новый и быстрый способ обработки маршрутов на плотность (но нужно сделать экспорт в json напрямую)

--- 2gis_density_from_jsons.py
+++ 2gis_density_from_jsons.py
@@ -1,73 +1,70 @@
-import json
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 00:19:15 2020
+
+@author: gamma
+"""
 from os import listdir
+import pandas as pd
 from os.path import isfile, join
-# тут бы нормально так засесть и прибраться...
-parced_list=[]
-def parcing(inputlist, mypath):
-    print('processing: '+mypath)
-    for file_name in inputlist:
-        file = json.load(open(mypath+'\\routes\\'+file_name, encoding='utf-8'))
-    #    print(file['result']['items'][0]['directions'][0]['platforms'][0].keys())
-    #    print(file['result']['items'][0]['directions'][0]['platforms'][1])
+import json
+import csv
+
+def csv_proc(mypath): #processing csv
+    file=mypath+r'\\density_groupby.csv'
+    outputfile=mypath+r'\\density_out.csv'
+    reader = csv.reader(open(file, newline='', encoding='utf-8'), delimiter=';')
+    writer = csv.writer(open(outputfile, 'w'), delimiter=';')
+    for row in reader:
+        print(row)
+        if '),  LINESTRING(' not in row[0]:
+            name=row[1].split(',')
+            count=len(name)
+            row.insert(1, count)
+            writer.writerow(row)
+    
+def process_2g(inputlist, ctr, ctr1, df): #regions
+    for filename in inputlist:
+        print(ctr1, filename)
+        file = json.load(open(mypath+'\\'+filename, encoding='utf-8'))
         item = file['result']['items'][0]
-        #Загружаем данные по маршруту
-        #Приводим кольцевые маршруты к одному формату
         for d in item['directions']:
             if d['type'] == 'loop':
                 d['type'] = 'circular'
-        #фильтруем направления, чтобы исключить дополнительные маршруты
         item['directions'] = list(map(lambda x: x[1], list(filter(lambda x: x[1]['type'] != "additional" and not (x[1]['type'] == 'circular' and x[0] > 0), enumerate(item['directions'])))))
-        #Сортируем направления, чтобы сначала было прямое, затем обратное
         item['directions'].sort(key=lambda x: 0 if x['type'] == 'circular' else 1 if x['type'] == 'forward' else 2, reverse=False)
-        #Выгружаем геометрию маршрута
-        geometry = list(map(lambda x: x['geometry']['selection'],item['directions']))
-        for i in geometry:
-            str1=i.split(',')
-            parced_list.append(str1[11:-1])
-    output_wkt=[]
-    file_name=''
-    for a in parced_list:
-        for ab in range(len(a)-1):
-            counter=0
-            name=''
-            for c in parced_list:
-                for cd in range(len(c)-1):
-                    if a[ab]==c[cd] and a[ab+1]==c[cd+1]:
-                        geomcheck='LineString ('+a[ab]+','+a[ab+1]+')'
-                        if geomcheck not in str(output_wkt):
-                            for file_name in inputlist:
-#                            print(file_name)
-                                file=json.load(open(mypath+'\\routes\\'+file_name, encoding='utf-8'))
-                                item = file['result']['items'][0]
-                                #Загружаем данные по маршруту
-                                #Приводим кольцевые маршруты к одному формату
-                                for d in item['directions']:
-                                    if d['type'] == 'loop':
-                                        d['type'] = 'circular'
-                                        #фильтруем направления, чтобы исключить дополнительные маршруты
-                                item['directions'] = list(map(lambda x: x[1], list(filter(lambda x: x[1]['type'] != "additional" and not (x[1]['type'] == 'circular' and x[0] > 0), enumerate(item['directions'])))))
-                                        #Сортируем направления, чтобы сначала было прямое, затем обратное
-                                item['directions'].sort(key=lambda x: 0 if x['type'] == 'circular' else 1 if x['type'] == 'forward' else 2, reverse=False)
-                                #Выгружаем геометрию маршрута
-                                geometry = list(map(lambda x: x['geometry']['selection'],item['directions']))
-                                if a[ab] in str(geometry) and a[ab+1] in str(geometry):
-                                    if file_name[:-5] not in name:
-                                        name=name+' '+file_name[:-5]
-                                        counter=counter+1
-            if counter!=0:
-                string1='LineString ('+a[ab]+','+a[ab+1]+'); '+str(counter)+'; '+name
-                output_wkt.append(string1)
-                print(string1)
-            else:
-                print('already in list')
-    return output_wkt
+        geometry = list(map(lambda x: x['geometry']['selection'][11:-1],item['directions']))
+        cords=str(geometry).split(',')
+        for cord in range(len(cords)-1):
+            cordss='LINESTRING('+str(cords[cord]).replace("['", '')+', '+str(cords[cord+1]).replace("']", "")+') '
+            df.loc[ctr] = [cordss, filename[:-5]]
+            ctr=ctr+1
+        ctr1+=1
+    return df
+
+def process(inputlist, ctr, ctr1, df): #moscow
+    for filename in inputlist:
+        file=open(mypath+'\\renamed\\'+filename, 'r')
+        print(ctr1, filename)
+        data=file.read()
+        str1=data[11:-1]
+        cords=str1.split(',')
+        for cord in range(len(cords)-1):
+            cordss='LINESTRING('+str(cords[cord])+', '+str(cords[cord+1])+') '
+            df.loc[ctr] = [cordss, filename[:-4]]
+            ctr=ctr+1
+        ctr1+=1
+    return df
 
 
-mypath=r'D:\0_density\Chelyabinsk'
-inputlist = [f for f in listdir(mypath+'\\routes') if isfile(join(mypath+'\\routes', f))]
-
-output_wkt=parcing(inputlist, mypath)
-file=open(mypath+'.txt', 'w')
-for row in output_wkt:
-    file.write(str(row) + '\n')
-file.close()
+mypath=r'put/your/path'
+inputlist = [f for f in listdir(mypath) if isfile(join(mypath, f)) and '.json' in f]
+ctr=0
+ctr1=1
+df = pd.DataFrame(columns=['cords', 'name_of_transport'])
+df2=process_2g(inputlist, ctr, ctr1, df)
+df1=df2.groupby(by='cords', group_keys=False)['name_of_transport'].apply(lambda list: set(list))
+print(df1)
+path_or_buf=mypath+r'\\density_groupby.csv'
+df1.to_csv(path_or_buf, sep=';')
+csv_proc(mypath)
